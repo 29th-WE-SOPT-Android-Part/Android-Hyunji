ea1465dc1edc78dbc1e5dc84f8a0964ff6b52f97
hyunji <24hyunji@naver.com>
2021-10-09 10:23:48 +0900

    week1 commit

---
+++ app/src/main/java/com/example/androidseminar/SignInActivity.kt
@@ -0,0 +1,71 @@
+package com.example.androidseminar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.androidseminar.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivitySignInBinding.inflate(layoutInflater)
+        val intent1= Intent(this,HomeActivity::class.java)
+
+
+
+
+        activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val id = it.data?.getStringExtra("id")
+                Log.d("SignInActivity",id.toString())
+                binding.homeIdEdit.setText(id)
+
+                val pw = it.data?.getStringExtra("pw")
+                Log.d("SignInActivity",pw.toString())
+                binding.homePwEdit.setText(pw)
+
+
+            }
+            else{
+                Log.d("SignInActivity","result failed")
+            }}
+
+
+
+        binding.btnLogin.setOnClickListener {
+            if(canLogin()){
+                Toast.makeText(this,"안녕하세요 현지님",Toast.LENGTH_SHORT).show()
+                startActivity(intent1)
+            }
+            else{
+                Toast.makeText(this,"로그인 실패",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnRegister.setOnClickListener{
+            activityResultLauncher.launch(Intent(this,SignUpActivity::class.java)) //TODO 이부분 수정 -> 여기 때문에 signUpActivity가 먼저 뜨는듯
+
+        }
+
+        setContentView(binding.root)
+    }
+
+    fun canLogin():Boolean{
+        return(binding.homeIdEdit.text.toString().isNotEmpty() && binding.homePwEdit.text.toString().isNotEmpty())
+
+    }
+
+
+}
